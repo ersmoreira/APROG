494407f81498de8934b61e2636cdd7b11e9aefb6
Edgar Moreira <ersmoreira@gmail.com>
2021-11-18 20:51:32 +0000

    Start implementing PrimoTempoInteiro

---
+++ src/Mooshak/Modularizacao_Semana2/Z_PrimoTempoInteiro.java
@@ -0,0 +1,48 @@
+package Mooshak.Modularizacao_Semana2;
+
+import java.util.Scanner;
+
+public class Z_PrimoTempoInteiro {
+
+    public static void main(String[] args) {
+        int numero1, numero2;
+        int quantidade = 0;
+        Scanner sc = new Scanner(System.in);
+
+        numero1 = sc.nextInt();
+        numero2 = sc.nextInt();
+
+        for (int i = minimo(numero1, numero2); i <= maximo(numero1, numero2); i++) {
+            if (checkNumeroPrimo(i)){
+                quantidade++;
+                System.out.println(i);
+            }
+        }
+        System.out.printf("(%d)\n", quantidade);
+    }
+
+    public static boolean checkNumeroPrimo(int numero) {
+        for (int i = 2; i < numero; i++) {
+            if (numero % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static int minimo(int valor1, int valor2) {
+        if (valor1 < valor2) {
+            return valor1;
+        } else {
+            return valor2;
+        }
+    }
+
+    public static int maximo(int valor1, int valor2) {
+        if (valor1 > valor2) {
+            return valor1;
+        } else {
+            return valor2;
+        }
+    }
+}
