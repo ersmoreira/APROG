cc7bfe4bae4e262c8d39eb50d94bdedd1cdcda4b
Edgar Moreira <ersmoreira@gmail.com>
2021-11-23 19:41:46 +0000

    Implementacao do exercicio Essencial X

---
+++ src/Mooshak/Essencial/X_Relogio.java
@@ -0,0 +1,18 @@
+package Mooshak.Essencial;
+
+import java.util.Scanner;
+
+public class M_SequenciaFibonacci {
+
+    public static void main(String[] args) {
+        int n1= 0, n2 = 0, n3, quantidade=0;
+        Scanner sc = new Scanner(System.in);
+        int numero = sc.nextInt();
+
+        for (int i = 0; i < ; i++) {
+            
+        }
+
+    }
+
+}
