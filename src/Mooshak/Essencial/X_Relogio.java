24e007a1f5a5bc0ef8d4a95b545eea60fa757e55
Edgar Moreira <ersmoreira@gmail.com>
2021-11-25 22:05:26 +0000

    FIX X_Relogio

--- src/Mooshak/Essencial/X_Relogio.java
+++ src/Mooshak/Essencial/X_Relogio.java
@@ -2,15 +2,27 @@ package Mooshak.Essencial;
 
 import java.util.Scanner;
 
-public class M_SequenciaFibonacci {
+public class X_Relogio {
 
     public static void main(String[] args) {
-        int n1= 0, n2 = 0, n3, quantidade=0;
+        int n;
         Scanner sc = new Scanner(System.in);
-        int numero = sc.nextInt();
+        n = sc.nextInt();
 
-        for (int i = 0; i < ; i++) {
-            
+        if (n == 1) {
+            System.out.println("Tag Heuer");
+        } else if (n == 2) {
+            System.out.println("Rolex");
+        } else if (n == 3) {
+            System.out.println("Omega");
+        } else if (n == 4) {
+            System.out.println("Cartier");
+        } else if (n == 5) {
+            System.out.println("Bvlgari");
+        } else if (n == 6) {
+            System.out.println("Raymond Weil");
+        } else {
+            System.out.println("Marca invalida");
         }
 
     }
