518b30710c4a3a4b8d2479a61bbc6dfe4787e12c
Edgar Moreira <ersmoreira@gmail.com>
2021-11-25 22:45:03 +0000

    Adicionar exercio Arrays D

---
+++ src/Mooshak/Arrays/D_FrequenciaNotas.java
@@ -0,0 +1,38 @@
+package Mooshak.Arrays;
+
+import java.util.Scanner;
+
+public class D_FrequenciaNotas {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int quantidadeAlunos = sc.nextInt();
+        int[] notas = lerNotas(sc, quantidadeAlunos);
+        imprimeEscala(notas);
+
+    }
+
+    public static int[] lerNotas(Scanner sc, int quantidadeAlunos) {
+        int[] notas = new int[21];
+        int valor = 0;
+
+        for (int i = 1; i <= quantidadeAlunos; i++) {
+            do {
+                valor = sc.nextInt();
+            } while (valor < 0 || valor > 20);
+            notas[valor]++;
+        }
+        return notas;
+    }
+
+    public static void imprimeEscala(int[] notas){
+        for (int i = 0; i <= 20 ; i++) {
+            System.out.printf("%d %d\n", i, notas[i]);
+        }
+    }
+}
+
+
+
+
