05ef80b6859f11660bd57f0823323e2a69265311
Edgar Moreira <ersmoreira@gmail.com>
2022-02-01 18:37:29 +0000

    Exame - Exercicio 3

--- src/exameSemestre1.java
+++ src/exameSemestre1.java
@@ -1,15 +1,16 @@
 public class exameSemestre1 {
 
+    final static int COLUNA_PRECO = 1;
+    final static int COLUNA_DURACAO = 0;
+
     public static void main(String[] args) {
         String[] musics = {"On my way", "Memories", "Perfect", "Havana"};
         double[][] info = {{201.6, 3.25}, {189, 2.80}, {256.8, 2.75}, {202.8, 2.35}};
-
 
     }
 
     // Exercicio 1
     public static void findMostExpensiveMusic(String[] musica, double[][] informacao) {
-        final int COLUNA_PRECO = 1;
         int indiceMaximo = 0;
         int totalMusicas = musica.length;
         double preco = 0, precoMaximo = 0;
@@ -18,10 +19,27 @@ public class exameSemestre1 {
             preco = informacao[linha][COLUNA_PRECO];
             if (preco >= precoMaximo) {
                 indiceMaximo = linha;
-                precoMaximo =
+                precoMaximo = preco;
             }
         }
 
         return musica[indiceMaximo];
     }
+
+    // Exercicio 3
+    public static double calculateBiggestDifference(double[][] informacao) {
+        double diferenca = 0, diferencaMaxima = 0;
+        int totalMusicas = informacao.length;
+
+        for (int linha = 1; linha < totalMusicas; linha++) {
+            diferenca = Math.abs(informacao[linha][COLUNA_DURACAO] - informacao[linha - 1][COLUNA_DURACAO]);
+            if (diferenca > diferencaMaxima) {
+                diferencaMaxima = diferenca;
+            }
+        }
+
+        return diferencaMaxima;
+    }
+
+
 }
